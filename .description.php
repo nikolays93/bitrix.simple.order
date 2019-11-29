b16d8690ff2cc04717cb54d9c1f1eca894f3b92b
nikolays93 <nikolays93@ya.ru>
2019-11-29 17:04:36 +0400

    Init new idea.

---
+++ .description.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => 'Быстрый заказ',
+	"DESCRIPTION" => 'Реализация функционала "заказ в один клик"',
+	"ICON" => "/images/icon.gif",
+	"SORT" => 10,
+	"CACHE_PATH" => "Y",
+	"PATH" => array(
+		"ID" => "Персональные решения",
+	),
+);
